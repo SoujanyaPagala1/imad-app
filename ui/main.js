228a76427931f0c10ee017651ecca0cc67f80070
Soujanya <30731426+SoujanyaPagala1@users.noreply.github.com>
2017-08-09 22:19:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,1 @@
 console.log('Loaded!');
-
-//Change the text of main-text div
-var element = document.getElementById('main-text');
-
-element.innerHTML = "new value"
-
-//move the image
-var marginLeft = 0;
-var img = document.getElementById('modi');
-function moveRight(){
-    marginLeft = marginLeft + 10;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function() {
-    var interval = setInterval(moveRight,100);
-}
